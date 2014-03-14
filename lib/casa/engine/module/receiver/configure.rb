e30569bfdd9543e1e83c7044694d5ab40df08564
Eric Bollens <eric@eb.io>
2014-03-13 18:42:33 -0700

    Always load tables on start

--- lib/casa/engine/module/receiver/configure.rb
+++ lib/casa/engine/module/receiver/configure.rb
@@ -8,11 +8,14 @@ module CASA
 
       configure do
 
+        settings.set(:has_loaded_tables, false) unless settings.respond_to?(:has_loaded_tables)
+
         settings.set(:receive_in_job, CASA::Engine::Support::ScheduledJob.new(:every, settings.jobs['intervals']['receive_in']) do
 
-          if CASA::Engine::Job::ReceiveIn.new(settings).execute
+          if CASA::Engine::Job::ReceiveIn.new(settings).execute or !settings.has_loaded_tables
             adj_in_to_local_job.execute if adj_in_to_local_job
             adj_in_to_adj_out_job.execute if adj_in_to_adj_out_job
+            settings.set :has_loaded_tables, true
           end
 
         end)
